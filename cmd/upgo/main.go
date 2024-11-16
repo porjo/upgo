dee967edb467a5dfa386ff40c8302c31c3b6ca3f
Ian Bishop <1296987+porjo@users.noreply.github.com>
2024-11-16 15:31:02 +1000

    get transactions

--- cmd/upgo/main.go
+++ cmd/upgo/main.go
@@ -22,4 +22,11 @@ func main() {
 
 	fmt.Printf("resp %+v\n", resp)
 
+	trans, err := c.GetTransactions(context.TODO(), "d63c429b-3538-46c0-bafb-2a26d0f34029")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("trans %+v\n", trans)
+
 }
--- upgo.go
+++ upgo.go
@@ -1,8 +1,11 @@
 package upgo
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
 	"fmt"
+	"html/template"
 	"net/http"
 	"os"
 
@@ -10,11 +13,13 @@ import (
 )
 
 const (
-	ServerURL = "https://api.up.com.au/api/v1"
+	ServerURL       = "https://api.up.com.au/api/v1"
+	TransactionsURL = ServerURL + "/accounts/{{ .accountID }}/transactions"
 )
 
 type Client struct {
-	client *oapi.ClientWithResponses
+	client   http.Client
+	upClient *oapi.ClientWithResponses
 }
 
 func NewClient() (*Client, error) {
@@ -24,29 +29,29 @@ func NewClient() (*Client, error) {
 		return nil, fmt.Errorf("environment variable API_TOKEN not set")
 	}
 
-	hc := http.Client{}
-	rt := getRTWithHeader(hc.Transport)
-	rt.Set("Authorization", "Bearer "+token)
-	hc.Transport = rt
-
 	var err error
 	c := &Client{}
 
-	c.client, err = oapi.NewClientWithResponses(ServerURL, oapi.WithHTTPClient(&hc))
+	c.client = http.Client{}
+	wh := getRTWithHeader(c.client.Transport)
+	wh.Set("Authorization", "Bearer "+token)
+	c.client.Transport = wh
+
+	c.upClient, err = oapi.NewClientWithResponses(ServerURL, oapi.WithHTTPClient(&c.client))
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("error getting client: %w", err)
 	}
 
-	_, err = c.client.GetUtilPing(context.TODO())
+	_, err = c.upClient.GetUtilPing(context.TODO())
 	if err != nil {
-		return nil, fmt.Errorf("error pinging API %w", err)
+		return nil, fmt.Errorf("error pinging API: %w", err)
 	}
 
 	return c, nil
 }
 
 func (c *Client) GetAccounts(ctx context.Context) ([]oapi.AccountResource, error) {
-	resp, err := c.client.GetAccountsWithResponse(ctx, &oapi.GetAccountsParams{})
+	resp, err := c.upClient.GetAccountsWithResponse(ctx, &oapi.GetAccountsParams{})
 	if err != nil {
 		return nil, err
 	}
@@ -55,4 +60,82 @@ func (c *Client) GetAccounts(ctx context.Context) ([]oapi.AccountResource, error
 	}
 
 	return resp.JSON200.Data, nil
+}
+
+/*
+func (c *Client) GetTransactions(ctx context.Context, accountID string) ([]oapi.TransactionResource, error) {
+	resp, err := c.upClient.GetTransactionsWithResponse(ctx, &oapi.GetTransactionsParams{})
+	if err != nil {
+		return nil, err
+	}
+	if resp.StatusCode() != http.StatusOK {
+		return nil, fmt.Errorf("error getting transactions. Expected HTTP 200 but received %d", resp.StatusCode())
+	}
+
+	if resp.JSON200 == nil {
+		return []oapi.TransactionResource{}, nil
+	}
+
+	transactions := resp.JSON200.Data
+
+	if resp.JSON200.Links.Next != nil {
+		t, err := c.getTransactions(ctx, *resp.JSON200.Links.Next)
+		if err != nil {
+			return nil, err
+		}
+
+		transactions = append(transactions, t...)
+
+	}
+
+	return transactions, nil
+}
+*/
+
+func (c *Client) GetTransactions(ctx context.Context, accountID string) ([]oapi.TransactionResource, error) {
+
+	var tpl bytes.Buffer
+
+	templ := template.Must(template.New("getTransactions").Parse(TransactionsURL))
+	err := templ.Execute(&tpl, map[string]interface{}{
+		"accountID": accountID,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return c.getTransactions(ctx, tpl.String())
+}
+
+func (c *Client) getTransactions(ctx context.Context, url string) ([]oapi.TransactionResource, error) {
+
+	req, err := http.NewRequestWithContext(ctx, "GET", url, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	r, err := c.client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer r.Body.Close()
+
+	ltr := &oapi.ListTransactionsResponse{}
+	err = json.NewDecoder(r.Body).Decode(ltr)
+	if err != nil {
+		return nil, err
+	}
+
+	transactions := ltr.Data
+
+	if ltr.Links.Next != nil {
+		t, err := c.getTransactions(ctx, *ltr.Links.Next)
+		if err != nil {
+			return nil, err
+		}
+		transactions = append(transactions, t...)
+	}
+
+	return transactions, nil
+
 }
